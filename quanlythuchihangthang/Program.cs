dd549dd6e5becd80f63eafe1b176857c07021491
unknown <thanhsonhoctap1255@gmail.com>
2024-04-12 15:14:36 +0700

    tạo menu cho class program, còn cần switch case lớn

--- quanlythuchihangthang/Program.cs
+++ quanlythuchihangthang/Program.cs
@@ -8,6 +8,43 @@ namespace quanly_thu_chi_trongthang
 {
     class Program
     {
+        public static void MenuTongThe()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("bang thao tac tong the: ");
+            Console.WriteLine("      ---------          ");
+            Console.WriteLine("0. thoat menu");
+            Console.WriteLine("1. thao tac voi ngan sach");
+            Console.WriteLine("2. thao tac voi cac danh muc");
+            Console.WriteLine("3. thao tac voi cac giao dich");
+            Console.WriteLine("=========================");
+        }
+
+        public static void MenuDanhMuc()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("bang thao tac danh muc: ");
+            Console.WriteLine("      ---------          ");
+            Console.WriteLine("0. thoat menu");
+            Console.WriteLine("1. thao tac voi ");
+            Console.WriteLine("2. thao tac voi ");
+            Console.WriteLine("3. thao tac voi ");
+            Console.WriteLine("=========================");
+        }
+
+        public static void MenuGiaoDich()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("bang thao tac cac giao dich: ");
+            Console.WriteLine("      ---------          ");
+            Console.WriteLine("0. thoat menu");
+            Console.WriteLine("1. them giao dich");
+            Console.WriteLine("2. xuat ds giao dich");
+            Console.WriteLine("3. tinh tong theo danh muc");
+            Console.WriteLine("4. xoa giao dich ra khoi dnh sach");
+            Console.WriteLine("=========================");
+        }
+
         static void Main(string[] args)
         {
             //GiaoDich gd = new GiaoDich();
@@ -18,7 +55,8 @@ namespace quanly_thu_chi_trongthang
             int chon;
             do
             {
-                Console.WriteLine("nhap gia tri de thuc hien: ");
+                MenuGiaoDich();
+                Console.WriteLine("nhap gia tri de thuc hien cac chuc nang giao dich: ");
                 chon = int.Parse(Console.ReadLine());
                 switch (chon)
                 {
@@ -56,5 +94,7 @@ namespace quanly_thu_chi_trongthang
 
             Console.ReadLine();
         }
+
+        
     }
 }

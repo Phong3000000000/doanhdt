8845e749123be50871ab1951684291149a8c709a
unknown <thanhsonhoctap1255@gmail.com>
2024-04-12 11:26:12 +0700

    class danhsachgiaodich voi method nhap xuat

--- quanlythuchihangthang/Program.cs
+++ quanlythuchihangthang/Program.cs
@@ -10,11 +10,13 @@ namespace quanly_thu_chi_trongthang
     {
         static void Main(string[] args)
         {
-            GiaoDich gd = new GiaoDich();
-            gd.tao1gd();
-            gd.xuat1gd();
+            //GiaoDich gd = new GiaoDich();
+            //gd.tao1gd();
+            //gd.xuat1gd();
 
-
+            DanhSachGiaoDich dsgd = new DanhSachGiaoDich();
+            dsgd.them_gd_vao_ds_giaodich();
+            dsgd.xuat_ds_giaodich();
 
 
             Console.ReadLine();

4fe10f94c22da49f4da6127621e3830bdff0a392
Phuong Nam <thaidagpnam@gmail.com>
2024-04-12 11:31:53 +0700

    basic dm chi tieu

--- quanlythuchihangthang/DanhMucAnUong.cs
+++ quanlythuchihangthang/DanhMucAnUong.cs
@@ -8,5 +8,15 @@ namespace quanly_thu_chi_trongthang
 {
     internal class DanhMucAnUong : DanhMucChiTieu
     {
+        string _LoaiAnUong;
+
+        public string LoaiAnUong { get => _LoaiAnUong; set => _LoaiAnUong = value; }
+
+        public DanhMucAnUong() : base() { }
+
+        public DanhMucAnUong(string loaianuong, string tendanhmuc, double mucngansachduocphanbo, double mucchitieu) : base(tendanhmuc, mucngansachduocphanbo, mucchitieu)
+        {
+            this.LoaiAnUong = loaianuong;
+        }
     }
 }
--- quanlythuchihangthang/DanhMucMuaSam.cs
+++ quanlythuchihangthang/DanhMucMuaSam.cs
@@ -8,5 +8,15 @@ namespace quanly_thu_chi_trongthang
 {
     internal class DanhMucMuaSam : DanhMucChiTieu
     {
+        string _CachThuc;
+
+        public string CachThuc { get => _CachThuc; set => _CachThuc = value; }
+
+        public DanhMucMuaSam() : base() { }
+
+        public DanhMucMuaSam(string cachthuc, string tendanhmuc, double mucngansachduocphanbo, double mucchitieu) : base(tendanhmuc, mucngansachduocphanbo, mucchitieu)
+        {
+            this.CachThuc = cachthuc;
+        }
     }
 }
